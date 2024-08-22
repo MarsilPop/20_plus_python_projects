8357d33ca5846e45da3411f05cafc9349d6245cf
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-08-22 16:14:37 +0200

    progress

---
+++ fizz.py
@@ -0,0 +1,11 @@
+# Fizz Buzz
+
+for number in range(1, 31):
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Fizz")
+    elif number % 3 == 0:
+        print("Buzz")
+    else:
+        print(number)
