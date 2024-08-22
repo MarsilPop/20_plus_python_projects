7dbfc21bceef6285afc50e25637785f8a040ba8e
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-08-22 16:34:46 +0200

    nested loops

--- fizz.py
+++ fizz.py
@@ -1,11 +1,20 @@
 # Fizz Buzz
 
-for number in range(1, 31):
-    if number % 15 == 0:
-        print("FizzBuzz")
-    elif number % 5 == 0:
-        print("Fizz")
-    elif number % 3 == 0:
-        print("Buzz")
-    else:
-        print(number)
+# for number in range(1, 31):
+#     if number % 15 == 0:
+#         print("FizzBuzz")
+#     elif number % 5 == 0:
+#         print("Fizz")
+#     elif number % 3 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
+
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter the symbol to use: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end=" ")
+    print()
